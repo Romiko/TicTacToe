a0cb456e2f6e49ebc4c4ae7a22b35fddfe21d319
Romiko Derbynew <romikov@gmail.com>
2019-10-24 21:05:41 +1100

    Added game

---
+++ TicTacToeGame/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame.Models
+{
+    internal class Player
+    {
+        public string Name { get; set; }
+        public Player Opponent { get; set; }
+        public bool IsPlaying { get; set; }
+        public bool WaitingForMove { get; set; }
+        public bool IsSearchingOpponent { get; set; }
+        public DateTime RegisterTime { get; set; }
+        public string Image { get; set; }
+        public string ConnectionId { get; set; }
+    }
+}
